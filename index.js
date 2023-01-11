92911a98928da345290d1e925853ee58f8a54dae
daniel matama <danmatama2020@gmail.com>
2023-01-11 11:01:13 +0300

    bug fix2

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const projects = [{
     and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and
     typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
     when an unknown printer took a galley of type and scrambled it 1960s with the relea`,
-    mechanizations: {
+  mechanizations: {
     mech0: 'Rubi on rails',
     mech1: 'css',
     mech2: 'JavaScript',
